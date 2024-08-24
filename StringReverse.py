ef7a8195d79d7dfb4db286f7957913ea8d8a9d5b
Sampath <sampathkumar.vemulakonda@gmail.com>
2024-08-24 07:20:43 +0530

    Added few more methods

--- StringReverse.py
+++ StringReverse.py
@@ -1,3 +1,22 @@
 # Reverse the String
+#Slicing Method
 string = "Madam"
 print(string[::-1])
+
+#ReverseMethod
+String = "Madam"
+print("".join(reversed(String)))
+
+#For Loop
+strReverse = ""
+for i in String:
+    strReverse = i + strReverse
+print(strReverse)
+
+#While loop
+strReverse = ""
+count = len(strReverse)
+while count > 0:
+    strReverse += string[count-1]
+    count= count - 1
+print(strReverse)
