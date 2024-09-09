152c56dabd2a31344a589a2ec7fcdecf0c5e8b4f
Sampath <sampathkumar.vemulakonda@gmail.com>
2024-09-09 20:28:37 +0530

    Uppercase program

---
+++ Uppercase.py
@@ -0,0 +1,8 @@
+# Function to convert a string to uppercase
+def to_uppercase(input_string):
+    return input_string.upper()
+
+# Test the function
+input_string = input("Enter a string: ")
+uppercase_string = to_uppercase(input_string)
+print("Uppercase version:", uppercase_string)
